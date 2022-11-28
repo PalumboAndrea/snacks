efa44a85544efb9fbda46393622a8cd9d9630098
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-11-28 13:01:33 +0100

    update

--- snack_3/js/myscript.js
+++ snack_3/js/myscript.js
@@ -2,14 +2,20 @@
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
  */
 
-const list = [1, 3, 5, 6, 10, 20, 50, 30, 140];
+const list = [1, 3, 5, 6, 10, 20, 50, 30, 140, 4, 10, 20, 50, 30, 40, 70];
+
+let sum = 0;
+
+for ( let i = 0 ; i < list.length ; i++ ){
+
+    if ( i % 2 === 1 ){
+        sum += list[i];
+    }
+    
+}
 
 
-
-
-
-
-
+console.log(sum);
 
 
 
